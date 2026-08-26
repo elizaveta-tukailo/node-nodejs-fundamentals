6e386a1d513597212e4191e70f6005bec068c9f8
Lizaveta Tukaila <lizatukailo@gmail.com>
2026-08-26 17:17:19 +0300

    feat: add restored script

--- src/fs/restore.js
+++ src/fs/restore.js
@@ -1,8 +1,47 @@
+import { readFile, mkdir, writeFile, readdir } from 'node:fs/promises';
+import { join, dirname, sep } from 'node:path';
+
 const restore = async () => {
-  // Write your code here
-  // Read snapshot.json
-  // Treat snapshot.rootPath as metadata only
-  // Recreate directory/file structure in workspace_restored
+  const restoredDirName = 'workspace_restored';
+  
+  try {
+    await readFile('snapshot.json');
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
+
+  try {
+    await readdir(restoredDirName);
+    throw new Error('FS operation failed');
+  } catch (error) {
+    if (error.message !== 'FS operation failed') {
+      if (error.code !== 'ENOENT') {
+        throw error;
+      }
+    } else {
+      throw error; 
+    }
+  }
+
+  await mkdir(restoredDirName);
+
+  const snapshotData = JSON.parse(await readFile('snapshot.json', 'utf-8'));
+
+  for (const entry of snapshotData.entries) {
+    const restoredPath = join(restoredDirName, entry.path);
+
+    if (entry.type === 'directory') {
+      await mkdir(restoredPath, { recursive: true });
+    } else if (entry.type === 'file') {
+      await mkdir(dirname(restoredPath), { recursive: true });
+      
+      const fileContentBuffer = Buffer.from(entry.content, 'base64');
+      
+      await writeFile(restoredPath, fileContentBuffer);
+    }
+  }
+
+  console.log(`Restore completed: ${snapshotData.entries.length} entries restored.`);
 };
 
 await restore();
